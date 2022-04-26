bed4935321e2be9a0d79d8e0277a45a4764c34c7
Roberto Contreras <contreras.java@gmail.com>
2022-04-25 22:53:19 -0500

    configuración de base de datos en  memoria H2

---
+++ src/test/java/intercorp/reto/clienteapi/ClienteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package intercorp.reto.clienteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
